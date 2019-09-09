6129114faf16b06f7ca13114b43b19380255fb22
Dahlia Welsh <dahliawelsh@gmail.com>
2019-09-09 21:40:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log('hello');
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() string ===);
- return "I can't hear you!";
-}
+  if (string.toLowerCase() === string) 
+    return "I can't hear you!";
+  }
