ffce928e608782cc36d2f59284d19710af86ccbd
Dahlia Welsh <dahliawelsh@gmail.com>
2019-09-09 21:47:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,3 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) 
     return "YES INDEED!";
 }
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) 
-    return "I can't hear you!";
-  }
